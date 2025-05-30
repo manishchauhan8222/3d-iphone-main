1f56ab2762c8243cbf6780845e8c090a6842a56d
Manish Chauhan <manishchauhan8222@gmail.com>
2025-05-30 15:13:46 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+import DisplaySection from "./components/DisplaySection";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import SoundSection from "./components/SoundSection";
+import WebgiViewer from "./components/WebgiViewer";
+import Loader from "./components/Loader";
+function App() {
+  const WebgiViewerRef = useRef();
+  const contentRef = useRef();
+  const handlePreview = () => {
+    WebgiViewerRef.current.triggerPreview()
+  }
+  return (
+    <div className="App">
+    <Loader />
+    <div ref={contentRef} id="content">
+      <Nav />
+      <Hero />
+      <SoundSection />
+      <DisplaySection triggerPreview= {handlePreview} />
+    </div>
+      <WebgiViewer contentRef={contentRef } ref={WebgiViewerRef} />
+    </div>
+  );
+}
+
+export default App;
